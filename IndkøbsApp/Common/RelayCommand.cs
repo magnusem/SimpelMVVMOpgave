0b243ba41f03e0da914544ddab9795f53b58e79f
Magnus Emil Mortensen <magn1596@edu.easj.dk>
2017-03-09 13:33:45 +0100

    første commit

---
+++ IndkøbsApp/Common/RelayCommand.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndkøbsApp.Common
+{
+    public class RelayCommand
+    {
+        
+        public RelayCommand(Action execute) : this(execute, null)
+        {
+
+        }
+        private Action methodToExecute = null;
+        private Func<bool> methodToDecectCanExecute = null;
+
+        public RelayCommand(Action methodToExecute, Func<bool> methodToDetectCanExecute)
+        {
+            this.methodToExecute = methodToExecute;
+            this.methodToDecectCanExecute = methodToDetectCanExecute;
+        }
+        public void Execute(object parameter)
+        {
+            this.methodToExecute();
+        }
+        public bool CanExecute(object parameter)
+        {
+            if (this.methodToDecectCanExecute == null)
+            {
+                return true;
+            }
+            else
+            {
+                return this.methodToDecectCanExecute();
+            }
+        }
+        public event EventHandler CanExecuteChanged;
+    }
+}
---
+++ IndkøbsApp/Model/Indkøb.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndkøbsApp.Model
+{
+    class Indkøb
+    {
+        public string Vare { get; set; }
+        public int Pris { get; set; }
+
+
+
+        public override string ToString()
+        {
+            return Vare + ", " + Pris + "kr.";
+        }
+
+
+    }
+}
